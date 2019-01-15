64b17ed301a629e130955682089a9f76e5fd705b
Cliffton86 <Cliff_ton@hotmail.com>
2019-01-15 17:45:43 +0800

    remove dept controller
    add in assigndeputycontroller

---
+++ ADTeam5/Bizlogic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ADTeam5.Models;
+namespace ADTeam5
+{
+    public class Bizlogic
+    {
+        SSISTeam5Context m = new SSISTeam5Context();
+
+        public List<String> GetStaffName
+        {
+            get
+            {
+                return m.User.Select<User, string>(c => c.Name).ToList<String>();
+
+            }
+        }
+    }
+}
---
+++ ADTeam5/Controllers/Department/AssignDeputyController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ADTeam5.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ADTeam5.Controllers.Department
+{
+    public class AssignDeputyController : Controller
+    {
+        private readonly SSISTeam5Context context;
+
+        public AssignDeputyController(SSISTeam5Context context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<User> u = new List<User>();
+            u = context.User.OrderBy(x => x.Name).ToList();
+            ViewBag.listofitems = u;
+            //ViewData["UserId"] = new SelectList(context.User, "UserId", "Name");
+            return View();
+        }
+    
+    }
+}
