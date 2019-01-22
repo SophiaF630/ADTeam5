ff7e6b99a8b34c6ba7c8342d0aaf1ac4b8ab6505
happysoup <45652524+happysoup@users.noreply.github.com>
2019-01-22 10:52:47 +0800

    Layout changes

--- ADTeam5/Startup.cs
+++ ADTeam5/Startup.cs
@@ -30,8 +30,10 @@ namespace ADTeam5
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.ConfigureApplicationCookie(options => options.LoginPath = "/Account/LogIn");
+
             services.Configure<CookiePolicyOptions>(options =>
-            {
+            { 
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
