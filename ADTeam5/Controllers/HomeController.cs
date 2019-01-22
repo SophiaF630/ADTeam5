9dba626201c80edf07c1e0404cd3201c5ff0daca
happysoup <45652524+happysoup@users.noreply.github.com>
2019-01-22 13:57:06 +0800

    small changes

--- ADTeam5/Controllers/HomeController.cs
+++ ADTeam5/Controllers/HomeController.cs
@@ -4,12 +4,15 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 using ADTeam5.Models;
 
 namespace ADTeam5.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
+
         public IActionResult Index()
         {
             return View();
