3533b0c9e4e9059d940ff519570d06d22a44056b
SophiaF630 <fengyuxi630@gmail.com>
2019-01-28 13:46:05 +0800

    update PO

---
+++ ADTeam5/ViewModels/PurchaseOrderRecordDetails.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ADTeam5.ViewModels
+{
+    public class PurchaseOrderRecordDetails
+    {
+        [Display(Name = "Item Number")]
+        public string ItemNumber { get; set; }
+        public string ItemName { get; set; }
+        [Display(Name = "Quantity")]
+        public int Quantity { get; set; }
+    }
+}
