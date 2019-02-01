6f6fc5cca78f5f4a08fad16b68db31e842d266bb
Emelynn Ow <emelynnow@gmail.com>
2019-02-01 17:32:06 +0800

    New Request

---
+++ ADTeam5/ViewModels/TempNewRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ADTeam5.ViewModels
+{
+    public class TempNewRequest
+    {
+        [Display(Name = "No.")]
+        public int RowID { get; set; }
+        [Display(Name = "Item Number")]
+        public string ItemNumber { get; set; }
+        public string ItemName { get; set; }
+        [Display(Name = "Quantity")]
+        public int Quantity { get; set; }
+    }
+}
