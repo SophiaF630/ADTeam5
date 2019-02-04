d143e379e8d6828ee1d0f73a2d8e771cf7f63094
Emelynn Ow <emelynnow@gmail.com>
2019-02-05 02:01:58 +0800

    Edited ViewRequest

---
+++ ADTeam5/ViewModels/ViewRequestDetails.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ADTeam5.ViewModels
+{
+    public class ViewRequestDetails
+    {
+        public string rrid { get; set; }
+        public string itemName { get; set; }
+        public int quantity { get; set; }
+    }
+}
